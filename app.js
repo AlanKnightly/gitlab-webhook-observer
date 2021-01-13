452ee774f6543ef17a3bfe3a9ff06d7d056d1e28
shenjiangping <shenjiangping@sdbattery.com>
2021-01-13 13:19:52 +0800

    feat: update handler

--- app.js
+++ app.js
@@ -1,79 +1,13 @@
 var express = require('express');
 
-const R = require('ramda');
+
 const app = express();
 const PORT = 80;
-const projHookMap = require("./botWebhooks.json");
-var expressWinston = require('express-winston');
-var winston = require('winston'); // for transports.Console
-
-
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+const HookHandler = require('./handleHook');
 
-// express-winston logger makes sense BEFORE the router
-app.use(expressWinston.logger({
-  transports: [
-    new winston.transports.File({ filename: 'gitlab-webhook-record', level: 'info' }),
-  ],
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.json()
-  )
-}));
-
-
-const resByEventType = (type, body) => {
-  const projName = R.pathOr('', ['project', 'name'], req.body); // 项目名称
-  const userName = R.pathOr('', ['user_name'], req.body);  //谁
-  switch (type) {
-    case 'issue':
-      const issueUrl = R.pathOr('', ['object_attributes', 'url'], req.body); // issue url
-      const issueTitle = R.pathOr('', ['object_attributes', 'title'], req.body); // issue title
-      return `[${projName}]\n
-        ${userName} 刚刚开了个issue [${issueTitle}](${issueUrl})
-        `;
-    case 'push':
-      const issueUrl = R.pathOr('', ['object_attributes', 'url'], req.body); // issue url
-      const issueTitle = R.pathOr('', ['object_attributes', 'title'], req.body); // issue title
-      return `[${projName}]\n
-        ${userName} 刚刚push了个分支 [${issueTitle}](${issueUrl})
-        `;
-  }
-
-};
-
-app.post('/gitlab/hook', function (req, res) {
-  //check: X-Gitlab-Token
-
-  const eventType = R.pathOr('', ['event_type'], req.body);  //事件类型
-  const Params = resByEventType(eventType, req.body);
-  // const gitlabEvent = req.get('X-Gitlab-Event'); //事件
-
-
-
-
-  const message = `${userName}刚刚在【${projName}】`;
-  const bots = projHookMap[projName];
-  // bots.map(n => {
-  //   axios.post(n, {
-  //     "msgtype": "markdown",
-  //     "markdown": {
-  //       "content": `【${gitlabEvent}】\n
-  //       类型:< font color=\"comment\">用户反馈</font>`
-  //     }
-  //   })
-  //     .then(function (response) {
-  //       console.log(response);
-  //     })
-  //     .catch(function (error) {
-  //       console.log(error);
-  //     });
-  // });
-  const payload = JSON.stringify(req.body);
-  console.log(payload);
-  // res.send(payload);
-});
+app.post('/gitlab/hook', HookHandler);
 
 app.listen(PORT, console.log(`listen on Port: ${PORT}`));
---
+++ handleHook.js
@@ -0,0 +1,64 @@
+const R = require('ramda');
+const projHookMap = require("./botWebhooks.json");
+
+
+export const HookHandler = (req, res) => {
+  const eventType = R.pathOr('', ['object_kind'], req.body);  //事件类型
+  const projName = R.pathOr('', ['project', 'name'], req.body); // 项目名称
+  const bots = projHookMap[projName];
+  let md = '';
+  // 根据event_type类型返回消息
+  switch (eventType) {
+    case 'merge_request':
+      const user = R.pathOr('', ['user', 'name'], req.body);
+      const srcBranch = R.pathOr('', ['object_attributes', 'source_branch'], req.body);
+      const targetBranch = R.pathOr('', ['object_attributes', 'target_branch'], req.body);
+      const url = R.pathOr('', ['object_attributes', 'url'], req.body);
+      const title = R.pathOr('', ['object_attributes', 'title'], req.body);
+      const state = R.pathOr('', ['object_attributes', 'state'], req.body);
+      const action = R.pathOr('', ['object_attributes', 'action'], req.body);
+      if (action == 'open') {
+        md = `<font color=\"info\">${user}</font> 刚刚提了个merge request [${title}](${url}) , 想将分支[${srcBranch}]合并到[${targetBranch}]`;
+      } else if (action == 'merge' && state == "merged") {
+        md = `<font color=\"info\">${user}</font> 刚刚将分支[${srcBranch}]合并到[${targetBranch}]`;
+      }
+      break;
+    case 'push':
+      const userName = R.pathOr('', ['user_name'], req.body);
+      const title = R.pathOr('', ['commits', 'title'], req.body);
+      const url = R.pathOr('', ['commits', 'url'], req.body);
+      md = ` <font color=\"info\">${user}</font>${userName} 刚刚在 ${projName} 上push了个分支 [${title}](${url})`;
+      break;
+    case 'note':
+      const user = R.pathOr('', ['user', 'name'], req.body);
+      const url = R.pathOr('', ['object_attributes', 'url'], req.body);
+      const noteableType = R.pathOr('', ['object_attributes', 'noteable_type'], req.body);
+      md = ` <font color=\"info\">${user}</font> ${userName} 在 ${noteableType} [${url}](${url}) 留下了评论 `;
+      break;
+    case 'issue':
+      const user = R.pathOr('', ['user', 'name'], req.body);
+      const issueUrl = R.pathOr('', ['object_attributes', 'url'], req.body);
+      const issueTitle = R.pathOr('', ['object_attributes', 'title'], req.body);
+      md = ` ${user} 刚刚在 ${projName} 开了个issue [${issueTitle}](${issueUrl}) `;
+      break;
+    default:
+      break;
+  }
+  if (md) {
+    bots.map(n => {
+      axios.post(n, {
+        "msgtype": "markdown",
+        "markdown": {
+          "content": md,
+        }
+      })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    });
+  }
+  res.send({ success: true });
+};
